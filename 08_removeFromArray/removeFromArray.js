a85ec4bd30d7484e73e41f58bd609d47e61fc654
Josh Parvis <joshuaparvis@gmail.com>
2025-08-15 14:10:56 -0400

    Create function to remove filtered number

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, num) {
+  return arr.filter((number) => number !== num);
 };
+
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
