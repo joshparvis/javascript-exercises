6709f5195df702576c95a7b4ea4342b49dfe6f8c
Josh Parvis <joshuaparvis@gmail.com>
2025-08-15 14:26:56 -0400

    Create function to add all numbers between arg

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+  let sum = 0;
 
+  if (numOne > numTwo) {
+    temp = numOne;
+    numOne = numTwo;
+    numTwo = temp; 
+  }
+
+  for (i = numOne; i < numTwo + 1; i++) {
+    sum += i;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
